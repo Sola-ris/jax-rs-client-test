99f0ad5f16b58af049fdbdfb3177d6a4c747ffe2
Sola-ris <190788035+Sola-ris@users.noreply.github.com>
2025-01-04 11:26:33 +0100

    Validate input in ExpectedCount.

--- src/main/java/io/github/solaris/jaxrs/client/test/request/ExpectedCount.java
+++ src/main/java/io/github/solaris/jaxrs/client/test/request/ExpectedCount.java
@@ -5,6 +5,12 @@ public final class ExpectedCount {
     private final int max;
 
     private ExpectedCount(int min, int max) {
+        if (min < 0) {
+            throw new IllegalArgumentException("'min' must be >= 0");
+        }
+        if (max < min) {
+            throw new IllegalArgumentException("'min' must be > 'max'");
+        }
         this.min = min;
         this.max = max;
     }
@@ -26,14 +32,23 @@ public final class ExpectedCount {
     }
 
     public static ExpectedCount times(int count) {
+        if (count < 1) {
+            throw new IllegalArgumentException("'count' must be >= 1");
+        }
         return new ExpectedCount(count, count);
     }
 
     public static ExpectedCount min(int min) {
+        if (min < 1) {
+            throw new IllegalArgumentException("'min' must be >= 1");
+        }
         return new ExpectedCount(min, Integer.MAX_VALUE);
     }
 
     public static ExpectedCount max(int max) {
+        if (max < 1) {
+            throw new IllegalArgumentException("'max' must be >= 1");
+        }
         return new ExpectedCount(1, max);
     }
 
